16a2c7d2945d7e252d6ac5e70fcd897de7710597
Prithvi <prithvin19@gmail.com>
2023-07-31 00:39:14 +0530

    clarity

--- app/head.tsx
+++ app/head.tsx
@@ -23,6 +23,13 @@ export default function Head() {
           gtag('config', 'G-32W93HZEP8');
         `}
       </Script>
+      <Script id="clarity">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "i7hzoh7g3e")`}
+      </Script>
     </>
   );
 }
